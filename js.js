24ca8f7065412c2bfca6992730302b9856eded79
Svetlana Malikova <s.u.malikova@gmail.com>
2016-05-16 23:53:12 +0300

    Создан игровой цикл. Создан второй массив.

--- js.js
+++ js.js
@@ -1,12 +1,39 @@
+"use strict";
+
+
+var ONE_FRAME_TIME = 1000 / 60;
+
+var mainloop = function() {
+	updateGame();
+	draw();
+};
+
+setInterval(mainloop, ONE_FRAME_TIME);
+
+
 var universe = [];
 
 for (var i = 0; i < 900; i++) {
 	universe[i] = [];
+	
 	for (var j = 0; j < 600; j++) {
 		universe[i][j] = Math.floor(Math.random() * 2);
 	}
 }
-	
+
+var nextUniverse = universe;
+
+for (var j = 0; j < nextUniverse[j].length; j++) {
+		
+		for (var i = 0; i < nextUniverse.length; i++) {
+			
+			if ((nextUniverse[i][j] == 1) && (nextUniverse[i+1][j+1] == 1)) {
+				nextUniverse[i][j] = 0;
+			}
+		}
+	}
+
+
 
 function draw() {
 	var canvas  = document.getElementById("canvas");
@@ -25,19 +52,18 @@ function draw() {
 
 function point(context,universe) {
 	
-	for (j = 0; j < universe[j].length; j++) {
+	for (var j = 0; j < universe[j].length; j++) {
 		
-		for (i = 0; i < universe.length; i++) {
+		for (var i = 0; i < universe.length; i++) {
 			
 			if (universe[i][j] == 1) {
 				var x = i * 2;
 				var y = j * 2;
-				context.fillStyle   = '#000'; 
+				context.fillStyle = '#000'; 
 				context.fillRect(x,y,2,2);
 			}
 		}
 	}
 }
 				
-window.onload = draw;
 
