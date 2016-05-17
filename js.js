a96eb85a966725dfda4387dc907c02e4f076fd0d
Svetlana Malikova <s.u.malikova@gmail.com>
2016-05-17 23:30:43 +0300

    Рефакторинг. Начало разработки игровой логики

--- js.js
+++ js.js
@@ -3,34 +3,30 @@
 
 var ONE_FRAME_TIME = 1000 / 60;
 
+var universe = newUniverse(300, 300);
+var generation = 0;
+
 var mainloop = function() {
-	createUniverse(universe);
-	draw();
+	universe = nextUniverse(universe);
+	draw(universe);
 };
 
 setInterval(mainloop, ONE_FRAME_TIME);
 
 
-var universe = [];
+function newUniverse(x, y) {
+	var universe = new Array(x);
 
-
-function newUniverse(universe) {
-	
-	for (var i = 0; i < 900; i++) {
-		universe[i] = [];
-
-		for (var j = 0; j < 600; j++) {
-			universe[i][j] = 0;
-		}
+	for (var i = 0; i < universe.length; i++) {
+		universe[i] = new Array(y);
 	}
+
+	return universe;
 }
 
 
-function createUniverse(universe) {
-	newUniverse(universe);
-	
+function fillUniverse(universe) {
 	for (var i = 0; i < universe.length; i++) {
-				
 		for (var j = 0; j < universe[0].length; j++) {
 			universe[i][j] = Math.floor(Math.random() * 2);
 		}
@@ -38,7 +34,25 @@ function createUniverse(universe) {
 }
 
 
-function draw() {
+function nextUniverse(universe) {
+	if (generation++ == 0) {
+		fillUniverse(universe);
+		return universe;
+	}
+
+	var universe2 = newUniverse(universe.length, universe[0].length);
+
+	for (var i = 0; i < universe2.length; i++) {
+		for (var j = 0; j < universe2[0].length; j++) {
+			universe2[i][j] = universe[i][j];
+		}
+	}
+
+	return universe2;
+}
+
+
+function draw(universe) {
 	var canvas  = document.getElementById("canvas");
 	canvas.width  = universe.length * 2; 
 	canvas.height = universe[0].length * 2; 
@@ -48,7 +62,7 @@ function draw() {
 	context.fillRect(0, 0, canvas.width, canvas.height);	
 		
 	context.beginPath();
-	point(context,universe);
+	point(context, universe);
 	context.stroke();	
 }
 
